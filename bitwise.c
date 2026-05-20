23c4b3ae9ccc25da1c025f855b3de782a569669b
Pavanakumar Devadiga <pavanakumarharigadde@gmail.com>
2026-05-20 20:26:19 +0530

    Create bitwise.c
    Added a C program file

---
+++ bitwise.c
@@ -0,0 +1,33 @@
+/*C program to learn bitwise operators*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+void calculate_the_maximum(int n, int k) {
+  int max_and=0, max_or=0, max_xor=0;
+  for(int i=1; i<=n; i++){
+    for(int j=i+1; j<=n; j++){
+        int and_val= i&j;
+        int or_val = i|j;
+        int xor_val = i^j;
+        
+        if(and_val<k && and_val>max_and)
+            max_and = and_val;
+        
+        if(or_val<k && or_val>max_or)
+            max_or = or_val;
+            
+        if(xor_val<k && xor_val>max_xor)
+            max_xor = xor_val;
+        
+    }
+  }
+  printf("%d\n%d\n%d", max_and, max_or, max_xor);
+}
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+    return 0;
+}
